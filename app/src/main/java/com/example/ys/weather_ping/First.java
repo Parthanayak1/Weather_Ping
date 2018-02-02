29af36fe8be181a00ac614182cf274cca5869b0b
YS <yogendrasingh890@gmail.com>
2018-02-02 15:48:27 +0530

    Final WeatherPing(Basic).Fixed various bugs.

---
+++ app/src/main/java/com/example/ys/weather_ping/First.java
@@ -0,0 +1,50 @@
+package com.example.ys.weather_ping;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class First extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.first);
+
+        Button al = (Button) findViewById(R.id.start);
+        Button a2= (Button) findViewById(R.id.button2);
+        al.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(First.this, Second.class);
+                First.this.startActivity(intent);
+                overridePendingTransition(R.anim.activityfadein,
+                        R.anim.splashfadeout);}
+
+        });
+
+        a2.setOnClickListener(new View.OnClickListener() {
+                                  public void onClick(View v) {
+                                      Intent i = new Intent(First.this, About.class);
+                                      startActivity(i);
+                                      return;
+                                  }
+        });
+
+
+    }
+}
+
+/*public void onStart(View v)
+    {
+        if(v.getId() == R.id.start)
+        {
+            Intent i  = new Intent(First.this, Second.class);
+            startActivity(i);
+        }
+    }
+}
+  */
